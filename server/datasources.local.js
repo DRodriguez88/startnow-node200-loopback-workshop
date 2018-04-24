f8f36b47a1970c57ca16dd82778eb0b452af2fca
Dj <rodriguezdanilo88@gmail.com>
2018-04-23 19:37:47 -0700

    production

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,24 +1,16 @@
-// module.exports = {
-//     "db": {
-//       "name": "db",
-//       "connector": "memory"
-//     },
-//     "MongoDB": {
-//       "host": "",
-//       "port": 0,
-//       "url": "",
-//       "database": "",
-//       "password": "",
-//       "name": "MongoDB",
-//       "user": "",
-//       "connector": "mongodb"
-//     }
-//   }
-
 module.exports = {
-  "MongoDB": {
-    "name": "MongoDB",
-    "connector": "mongodb",
-    "url": process.env.MONGODB_URI
+    "db": {
+      "name": "db",
+      "connector": "memory"
+    },
+    "MongoDB": {
+      "host": "",
+      "port": 0,
+      "url": "",
+      "database": "",
+      "password": "",
+      "name": "MongoDB",
+      "user": "",
+      "connector": "mongodb"
+    }
   }
-}
